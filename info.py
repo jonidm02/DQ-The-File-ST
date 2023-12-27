681ff31bac2e7fd475074ecbe3c783b5be89d2f5
Joni <152844226+jonidm02@users.noreply.github.com>
2023-12-27 15:48:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jokerefx2024:jokerefx2024@cluster0.ie9e2ny.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "jokerefx2024")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://joni:Joni2024@cluster0.ruociwj.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Joni2024")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
